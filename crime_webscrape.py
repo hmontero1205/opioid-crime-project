6f862dc402009536f16537a57f1527e19935dba6
Hans <hjm2133@columbia.edu>
2017-10-26 16:32:00 -0400

    cleaned up code, put web scrape in a function

--- crime_webscrape.py
+++ crime_webscrape.py
@@ -1,31 +1,37 @@
 import urllib.request
 from bs4 import BeautifulSoup
 
-crimes_committed = dict()
-search_city = "Birmingham"
-search_state = "Alabama"
-
-url = "http://www.city-data.com/crime/crime-" + search_city + "-" + search_state + ".html"
-page = urllib.request.urlopen(url)
-soup = BeautifulSoup(page, "html.parser")
-table = soup.find(id="crimeTab")            
-tds = table.find_all('td')
-
-
-for cell in tds:
-    if ".com" in str(cell.contents[0]):
-        break
+def extract_data(search_city, search_state):
+    crimes_committed = dict()
     
-    if "N/A" in str(cell.contents[0]):
-        continue
+    url = "http://www.city-data.com/crime/crime-" + search_city + "-" + search_state + ".html"
+    page = urllib.request.urlopen(url)
+    soup = BeautifulSoup(page, "html.parser")
     
-    if "<b>" in str(cell.contents[0]):
-        header = str(cell.contents[0].contents[0])
-    else:
-        num_committed = int(str(cell.contents[0].replace(",", "")))
-        if not(header in crimes_committed.keys()):
-            crimes_committed[header] = 0
+    #Data table
+    table = soup.find(id="crimeTab")            
+    tds = table.find_all('td')
+    
+    
+    #Extract data from crime stats website. 
+    for cell in tds:
+        if ".com" in str(cell.contents[0]):
+            break
         
-        crimes_committed[header] += num_committed
+        if "N/A" in str(cell.contents[0]):
+            continue
+        
+        if "<b>" in str(cell.contents[0]):
+            header = str(cell.contents[0].contents[0])
+        else:
+            num_committed = int(str(cell.contents[0].replace(",", "")))
+            if not(header in crimes_committed.keys()):
+                crimes_committed[header] = 0
+            
+            crimes_committed[header] += num_committed
+            
+    #Easily accessible data stored in a dictionary
+    return crimes_committed
 
-print(crimes_committed)
+#Test Code
+print(extract_data("New-York", "New-York"))
